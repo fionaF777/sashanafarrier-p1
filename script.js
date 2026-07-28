e283b55214eada9d7247e82b52f304b728f09e2f
fionaF777 <sashanafarrier@yahoo.com>
2026-07-27 22:10:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 console.log("My first website!");
+
+//Basic math operations
+let a = 10;
+let b = 5;
+
+console.log(a);
+console.log(b);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+let sum = a + b;
+
+console.log(sum);
+
+//String operations
+let first = "Hello";
+let second = "World";
+
+console.log(first + " " + second);
+console.log(a > b);
+console.log(a == b);
+
+//Build a simple calculator
+let num1 = 8;
+let num2 = 2;
+
+console.log("Add:", num1 + num2);
+console.log("Subtract:", num1 - num2);
+console.log("Multiply:", num1 * num2);
+console.log("Divide:", num1 / num2);
+
+//Modify calculator
+let price = 20;
+let tax = 0.1;
+let total = price + price * tax;
+
+console.log(total);
