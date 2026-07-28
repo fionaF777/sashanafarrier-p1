8ba5b7f638fcbb024f845f2f965ee9f7adf0236d
fionaF777 <sashanafarrier@yahoo.com>
2026-07-27 22:33:45 -0500

    Assignment 5

--- script.js
+++ script.js
@@ -4,12 +4,45 @@ console.log("My first website!");
 let a = 10;
 let b = 5;
 
-console.log(a);
-console.log(b);
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(add(8, 2));
+
+console.log(subtract(8, 2));
+
+console.log(multiply(8, 2));
+
+console.log(divide(8, 2));
+
+
+function formatName(name) {
+    return name.toUpperCase();
+}
+
+function cleanInput(text) {
+    return text.trim();
+}
+
+let input = " cherry ";
+
+if (cleanInput(input).length > 0) {
+    console.log(formatName(input));
+}
+
 
 let sum = a + b;
 
@@ -27,10 +60,24 @@ console.log(a == b);
 let num1 = 8;
 let num2 = 2;
 
-console.log("Add:", num1 + num2);
-console.log("Subtract:", num1 - num2);
-console.log("Multiply:", num1 * num2);
-console.log("Divide:", num1 / num2);
+function calculate(n1, n2) {
+  let add = n1 + n2;
+  let subtract = n1 - n2;
+  let multiply = n1 * n2;
+  let divide = n1 / n2;
+  return { add, subtract, multiply, divide };
+}
+
+function printResults() {
+    let results = calculate(num1, num2);
+    console.log("Results:", results);
+    console.log("Add:", results.add);
+    console.log("Subtract:", results.subtract);
+    console.log("Multiply:", results.multiply);
+    console.log("Divide:", results.divide);
+}
+
+printResults();
 
 //Modify calculator
 let price = 20;
